eb2e64c6206a35ccdf7696ec6bad4474d7c90cdc
KopalAg28 <91335151+KopalAg28@users.noreply.github.com>
2022-06-25 15:52:19 +0530

    Add files via upload

---
+++ compute_amplitude_liset.m
@@ -0,0 +1,11 @@
+function [amp]=compute_amplitude_liset(x)
+        for i=1:size(x,1)
+            if ~isempty(x{i,:}) 
+            amp{i} = URC_compute_ampLabel(x{i,:},2500);
+            else 
+                amp{i} = {};
+            end 
+        end
+ amp = amp.';
+end 
+   
