d3c07d4863fdf76ee2d101b35582b2ab9a167546
mpackorapp <56198300+mpackorapp@users.noreply.github.com>
2022-08-23 15:09:34 +0300

    Update find_diValue.py

--- Distances/find_diValue.py
+++ Distances/find_diValue.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 
 df = pd.read_excel("/home/irene/Documents/UMAP/latest_data.xlsx", usecols="C,D,E,F,G,H,I")
 
+
 def value_cmp(string,value):
     Amp=[];
     for i in range(df.shape[0]):
@@ -21,21 +22,21 @@ def value_cmp(string,value):
 trialsArray=[value_cmp("trial", 1),value_cmp("trial", 2),value_cmp("trial", 3),
              value_cmp("trial", 4),value_cmp("trial", 5),value_cmp("trial", 6)]
 
-ratsArray=[value_cmp("subject", 1),value_cmp("subject", 2),value_cmp("subject", 3),
-           value_cmp("subject", 4),value_cmp("subject", 6),value_cmp("subject", 7),
-           value_cmp("subject", 8),value_cmp("subject", 9)]
+ratsArray=[value_cmp("subject", 1),value_cmp("subject", 2),value_cmp("subject", 6),
+           value_cmp("subject", 9),value_cmp("subject", 3),value_cmp("subject", 4),
+           value_cmp("subject", 7),value_cmp("subject", 8)]
 
 daysArray=[value_cmp("condition.1", "or"),value_cmp("condition.1", "od"),value_cmp("condition.1", "con")]
 
 arrayDI=[df.loc[:,"DI_5min"],df.loc[:,"DI_10min"]]
 
-def find_logic(trial, rat, day):
+def find_logic(rat, day, trial):
     K=[]
     
-    for i in range(len(trial)):
-        for j in range(len(rat)):
-            for k in range(len(day)):
-                logicresult=trial[i]*rat[j]*day[k]
+    for i in range(len(rat)):
+        for j in range(len(day)):
+            for k in range(len(trial)):
+                logicresult=rat[i]*day[j]*trial[k]
                 K.append(logicresult)    
     
     return K
@@ -43,7 +44,7 @@ def find_logic(trial, rat, day):
 
 def outputDI():
     res=[]
-    logicresult=find_logic(trialsArray, ratsArray, daysArray)
+    logicresult=find_logic(ratsArray, daysArray,trialsArray)
     for i in range(144):
         for j in range(144):
             if (logicresult[i][j]==1):
