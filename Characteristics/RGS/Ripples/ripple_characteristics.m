6081b60c400f68b03839629bdeb8fa0ab68a71dc
seyhuni <huseyin.yanik@windowslive.com>
2022-09-16 09:51:32 +0300

    Update ripple_characteristics.m

--- Characteristics/RGS/Ripples/ripple_characteristics.m
+++ Characteristics/RGS/Ripples/ripple_characteristics.m
@@ -3,24 +3,17 @@ clearvars
 clc
 close all
 % x- Entropy; y- Mean Freq; z- Amplitude; l- AUC; q- Duration; p- Peak2Peak
-
-% cd('/media/genzel/genzel1/UMAP_NSD/')
-% cd('/media/genzel/genzel1/UMAP_Basic_OS/13')
-cd('/media/genzel/genzel1/RGS14_NSD/RGS_Rats/6')
-
 % prompt = {'Select a trial for comparison 1(presleep)-2(PT1)-3(PT2)-4(PT3)-5(PT4)-6(PT5.1)-7(PT5.2)-8(PT5.3)-9(PT5.4))'};
 % dlgtitle = 'Choose between (1-9)';
 % definput = {'9'};
 % ChosenPT = inputdlg(prompt,dlgtitle,[1 80],definput); %Chosen PT for Characteristics
 % m=str2double(ChosenPT{1, 1});
 
-m=1; %1(presleep)-2(PT1)-3(PT2)-4(PT3)-5(PT4)-6(PT5.1)-7(PT5.2)-8(PT5.3)-9(PT5.4)
-
-% OR 
-[OR,path]=uigetfile('ripple_waveforms_broadband*.mat','Select OR');
-File=fullfile(path,OR)
-ripple_waveform_broadband_total_OR=load(File);
-Ripples_OR_total=ripple_waveform_broadband_total_OR.ripple_waveform_broadband_total;
+% % OR 
+% [OR,path]=uigetfile('ripple_waveforms_broadband*.mat','Select OR');
+% File=fullfile(path,OR)
+% ripple_waveform_broadband_total_OR=load(File);
+% Ripples_OR_total=ripple_waveform_broadband_total_OR.ripple_waveform_broadband_total;
 % 
 % % HC
 % [HC,path]=uigetfile('ripple_waveforms_broadband*.mat','Select HC');
@@ -40,134 +33,314 @@ Ripples_OR_total=ripple_waveform_broadband_total_OR.ripple_waveform_broadband_to
 % ripple_waveform_broadband_total_OD=load(File);
 % Ripples_OD_total=ripple_waveform_broadband_total_OD.ripple_waveform_broadband_total;
 
-% [Conditions,path]=uigetfile('ripple_waveforms_broadband*.mat','MultiSelect','on');
-% 
-% OR=Conditions(find(contains(Conditions,'OR')))
-% FileOR=fullfile(path,OR{1,1})
-% ripple_waveform_broadband_total_OR=load(FileOR);
-% Ripples_OR_total=ripple_waveform_broadband_total_OR.ripple_waveform_broadband_total;
-% 
-% HC=Conditions(find(contains(Conditions,'HC')))
-% FileHC=fullfile(path,HC{1,1})
-% ripple_waveform_broadband_total_HC=load(FileHC);
-% Ripples_HC_total=ripple_waveform_broadband_total_HC.ripple_waveform_broadband_total;
-% 
-% CN=Conditions(find(contains(Conditions,'CN')))
-% FileCN=fullfile(path,CN{1,1})
-% ripple_waveform_broadband_total_CN=load(FileCN);
-% Ripples_CN_total=ripple_waveform_broadband_total_CN.ripple_waveform_broadband_total;
-% 
-% OD=Conditions(find(contains(Conditions,'OD')))
-% FileOD=fullfile(path,OD{1,1})
-% ripple_waveform_broadband_total_OD=load(FileOD);
-% Ripples_OD_total=ripple_waveform_broadband_total_OD.ripple_waveform_broadband_total;
 
 
+cd('/media/genzel/genzel1/UMAP_NSD/5')
+% cd('/media/genzel/genzel1/UMAP_Basic_OS/9')
+% cd('/media/genzel/genzel1/UMAP_Basic_OS/13/')
+
+m=9; %1(presleep)-2(PT1)-3(PT2)-4(PT3)-5(PT4)-6(PT5.1)-7(PT5.2)-8(PT5.3)-9(PT5.4)
+
+[Conditions,path]=uigetfile('ripple_waveforms_broadband*.mat','MultiSelect','on');
+
+% or_n, or_n_s and or_nn_s
+
+OR_NN_S=Conditions(find(contains(Conditions,'OR_NN_S')))
+if ~isempty(OR_NN_S)
+FileOR_NN_S=fullfile(path,OR_NN_S{1,1})
+ripple_waveform_broadband_total_OR_NN_S=load(FileOR_NN_S);
+Ripples_OR_NN_S_total=ripple_waveform_broadband_total_OR_NN_S.ripple_waveform_broadband_total;
+Ripples_OR_NN_S = Ripples_OR_NN_S_total{1, m}';
+else
+    Ripples_OR_NN_S={}; 
+end
+
+OR_N_S=Conditions(find(contains(Conditions,'OR_N_S')))
+if ~isempty(OR_N_S)
+FileOR_N_S=fullfile(path,OR_N_S{1,1})
+ripple_waveform_broadband_total_OR_N_S=load(FileOR_N_S);
+Ripples_OR_N_S_total=ripple_waveform_broadband_total_OR_N_S.ripple_waveform_broadband_total;
+Ripples_OR_N_S = Ripples_OR_N_S_total{1, m}';
+else
+    Ripples_OR_N_S={};
+end
+
+HC=Conditions(find(contains(Conditions,'HC')))
+if ~isempty(HC)
+FileHC=fullfile(path,HC{1,1})
+ripple_waveform_broadband_total_HC=load(FileHC);
+Ripples_HC_total=ripple_waveform_broadband_total_HC.ripple_waveform_broadband_total;
+Ripples_HC = Ripples_HC_total{1, m}';
+else
+    Ripples_HC={};
+end
+CN=Conditions(find(contains(Conditions,'CN')))
+if ~isempty(CN)
+FileCN=fullfile(path,CN{1,1})
+ripple_waveform_broadband_total_CN=load(FileCN);
+Ripples_CN_total=ripple_waveform_broadband_total_CN.ripple_waveform_broadband_total;
+Ripples_CN = Ripples_CN_total{1, m}';
+else
+    Ripples_CN={};
+end
+
+OD=Conditions(find(contains(Conditions,'OD')))
+if ~isempty(OD)
+FileOD=fullfile(path,OD{1,1})
+ripple_waveform_broadband_total_OD=load(FileOD);
+Ripples_OD_total=ripple_waveform_broadband_total_OD.ripple_waveform_broadband_total;
+Ripples_OD = Ripples_OD_total{1, m}';
+else
+   Ripples_OD={};
+end
+
+OR_N=Conditions(find(contains(Conditions,'OR_N')))
+if ~isempty(OR_N)
+FileOR_N=fullfile(path,OR_N{1,end})
+ripple_waveform_broadband_total_OR_N=load(FileOR_N);
+Ripples_OR_N_total=ripple_waveform_broadband_total_OR_N.ripple_waveform_broadband_total;
+Ripples_OR_N = Ripples_OR_N_total{1, m}';
+else
+   Ripples_OR_N={};
+end
+
+
+
+OR=Conditions(find(contains(Conditions,'OR')))
+if ~isempty(OR)
+FileOR=fullfile(path,OR{1,end})
+ripple_waveform_broadband_total_OR=load(FileOR);
+Ripples_OR_total=ripple_waveform_broadband_total_OR.ripple_waveform_broadband_total;
+Ripples_OR = Ripples_OR_total{1, m}';
+else
+    Ripples_OR={};
+end
 
 % Trial by Trial  
 % Ripples_HC = Ripples_HC_total{1, m}';
-Ripples_OR = Ripples_OR_total{1, m}';
+% Ripples_OR = Ripples_OR_total{1, m}';
 % Ripples_OD = Ripples_OD_total{1, m}';
 % Ripples_CN = Ripples_CN_total{1, m}';
+% Ripples_OR_N = Ripples_OR_N_total{1, m}';
+% Ripples_OR_N_S = Ripples_OR_N_S_total{1, m}';
+% Ripples_OR_NN_S = Ripples_OR_NN_S_total{1, m}';
 
 
-% si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_HC,'UniformOutput',false);
-% si_index= cell2mat(si_index);
-% si_index = ~logical(si_index);
-% Ripples_HC= Ripples_HC(si_index);
+if ~iscell(Ripples_HC)
+    if isnan(Ripples_HC)
+        Ripples_HC={};
+    elseif isempty(Ripples_HC)
+        Ripples_HC={};
+    end
+end
+if ~iscell(Ripples_OR)
+     if isnan(Ripples_OR)
+        Ripples_OR={};
+     elseif isempty(Ripples_OR)
+        Ripples_OR={};
+     end
+end
+if ~iscell(Ripples_OD)
+     if isnan(Ripples_OD)
+        Ripples_OD={};   
+     elseif isempty(Ripples_OD)
+         Ripples_OD={};
+     end
+end
+if ~iscell(Ripples_CN)
+     if isnan(Ripples_CN)
+         Ripples_CN={};
+     elseif isempty(Ripples_CN)
+         Ripples_CN={};         
+     end
+end
+if ~iscell(Ripples_OR_N)
+     if isnan(Ripples_OR_N)
+        Ripples_OR_N={};
+     elseif isempty(Ripples_OR_N)
+         Ripples_OR_N={};         
+     end
+end
+if ~iscell(Ripples_OR_N_S)
+     if isnan(Ripples_OR_N_S)
+        Ripples_OR_N_S={};
+     elseif isempty(Ripples_OR_N_S)
+         Ripples_OR_N_S={};                
+     end
+end
+if ~iscell(Ripples_OR_NN_S)
+     if isnan(Ripples_OR_NN_S)
+        Ripples_OR_NN_S={};
+     elseif isempty(Ripples_OR_NN_S)
+         Ripples_OR_NN_S={};        
+     end
+end
+
+si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_HC,'UniformOutput',false);
+si_index= cell2mat(si_index);
+si_index = ~logical(si_index);
+Ripples_HC= Ripples_HC(si_index);
 
 si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_OR,'UniformOutput',false);
 si_index = cell2mat(si_index);
 si_index = ~logical(si_index);
 Ripples_OR = Ripples_OR(si_index); 
 
-% si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_OD,'UniformOutput',false);
-% si_index= cell2mat(si_index);
-% si_index = ~logical(si_index);
-% Ripples_OD= Ripples_OD(si_index);
-% 
-% si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_CN,'UniformOutput',false);
-% si_index = cell2mat(si_index);
-% si_index = ~logical(si_index);
-% Ripples_CN = Ripples_CN(si_index); 
+si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_OD,'UniformOutput',false);
+si_index= cell2mat(si_index);
+si_index = ~logical(si_index);
+Ripples_OD= Ripples_OD(si_index);
+
+si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_CN,'UniformOutput',false);
+si_index = cell2mat(si_index);
+si_index = ~logical(si_index);
+Ripples_CN = Ripples_CN(si_index); 
+
+si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_OR_N,'UniformOutput',false);
+si_index = cell2mat(si_index);
+si_index = ~logical(si_index);
+Ripples_OR_N = Ripples_OR_N(si_index); 
+
+si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_OR_N_S,'UniformOutput',false);
+si_index = cell2mat(si_index);
+si_index = ~logical(si_index);
+Ripples_OR_N_S = Ripples_OR_N_S(si_index); 
+
+si_index = cellfun(@(equis) sum(isnan(equis)), Ripples_OR_NN_S,'UniformOutput',false);
+si_index = cell2mat(si_index);
+si_index = ~logical(si_index);
+Ripples_OR_NN_S = Ripples_OR_NN_S(si_index); 
 
 Wn1=[100/(2500/2) 300/(2500/2)]; % Cutoff=100-300 Hz
 [b1,a1] = butter(3,Wn1,'bandpass'); %Filter coefficients
+Ripples_OR_N = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_OR_N ,'UniformOutput',false);
+Ripples_OR_N_S = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_OR_N_S ,'UniformOutput',false);
+Ripples_OR_NN_S = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_OR_NN_S ,'UniformOutput',false);
 Ripples_OR = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_OR ,'UniformOutput',false);
-% Ripples_HC = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_HC ,'UniformOutput',false);
-% Ripples_OD = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_OD ,'UniformOutput',false);
-% Ripples_CN = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_CN ,'UniformOutput',false);
+Ripples_HC = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_HC ,'UniformOutput',false);
+Ripples_OD = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_OD ,'UniformOutput',false);
+Ripples_CN = cellfun(@(equis)filtfilt(b1,a1,equis),Ripples_CN ,'UniformOutput',false);
 
 % Entropy
 x_or = cellfun(@(equis) entropy(equis),Ripples_OR,'UniformOutput',false);
 x_or = vertcat(x_or{:});
-% x_hc = cellfun(@(equis) entropy(equis),Ripples_HC,'UniformOutput',false);
-% x_hc = vertcat(x_hc{:});
-% x_od = cellfun(@(equis) entropy(equis),Ripples_OD,'UniformOutput',false);
-% x_od = vertcat(x_od{:});
-% x_cn = cellfun(@(equis) entropy(equis),Ripples_CN,'UniformOutput',false);
-% x_cn = vertcat(x_cn{:});
+x_hc = cellfun(@(equis) entropy(equis),Ripples_HC,'UniformOutput',false);
+x_hc = vertcat(x_hc{:});
+x_od = cellfun(@(equis) entropy(equis),Ripples_OD,'UniformOutput',false);
+x_od = vertcat(x_od{:});
+x_cn = cellfun(@(equis) entropy(equis),Ripples_CN,'UniformOutput',false);
+x_cn = vertcat(x_cn{:});
+x_or_n = cellfun(@(equis) entropy(equis),Ripples_OR_N,'UniformOutput',false);
+x_or_n = vertcat(x_or_n{:});
+x_or_n_s = cellfun(@(equis) entropy(equis),Ripples_OR_N_S,'UniformOutput',false);
+x_or_n_s = vertcat(x_or_n_s{:});
+x_or_nn_s = cellfun(@(equis) entropy(equis),Ripples_OR_NN_S,'UniformOutput',false);
+x_or_nn_s = vertcat(x_or_nn_s{:});
+
+
 
 % Mean Freqs
 y_or = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_OR,'UniformOutput',false);
 y_or = vertcat(y_or{:});
-% y_hc = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_HC,'UniformOutput',false);
-% y_hc = vertcat(y_hc{:});
-% y_od = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_OD,'UniformOutput',false);
-% y_od = vertcat(y_od{:});
-% y_cn = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_CN,'UniformOutput',false);
-% y_cn = vertcat(y_cn{:});
+y_hc = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_HC,'UniformOutput',false);
+y_hc = vertcat(y_hc{:});
+y_od = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_OD,'UniformOutput',false);
+y_od = vertcat(y_od{:});
+y_cn = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_CN,'UniformOutput',false);
+y_cn = vertcat(y_cn{:});
+y_or_n = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_OR_N,'UniformOutput',false);
+y_or_n = vertcat(y_or_n{:});
+y_or_n_s = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_OR_N_S,'UniformOutput',false);
+y_or_n_s = vertcat(y_or_n_s{:});
+y_or_nn_s = cellfun(@(equis) (meanfreq(equis,2500)),Ripples_OR_NN_S,'UniformOutput',false);
+y_or_nn_s = vertcat(y_or_nn_s{:});
 
 % Amplitude
 z_or = cellfun(@(equis) max(abs(hilbert(equis))) ,Ripples_OR,'UniformOutput',false);
 z_or = vertcat(z_or{:});
-% z_hc = cellfun(@(equis) max(abs(hilbert(equis))),Ripples_HC,'UniformOutput',false);
-% z_hc = vertcat(z_hc{:});
-% z_od = cellfun(@(equis) max(abs(hilbert(equis))) ,Ripples_OD,'UniformOutput',false);
-% z_od = vertcat(z_od{:});
-% z_cn = cellfun(@(equis) max(abs(hilbert(equis))),Ripples_CN,'UniformOutput',false);
-% z_cn = vertcat(z_cn{:});
+z_hc = cellfun(@(equis) max(abs(hilbert(equis))),Ripples_HC,'UniformOutput',false);
+z_hc = vertcat(z_hc{:});
+z_od = cellfun(@(equis) max(abs(hilbert(equis))) ,Ripples_OD,'UniformOutput',false);
+z_od = vertcat(z_od{:});
+z_cn = cellfun(@(equis) max(abs(hilbert(equis))),Ripples_CN,'UniformOutput',false);
+z_cn = vertcat(z_cn{:});
+z_or_n = cellfun(@(equis) max(abs(hilbert(equis))) ,Ripples_OR_N,'UniformOutput',false);
+z_or_n = vertcat(z_or_n{:});
+z_or_n_s = cellfun(@(equis) max(abs(hilbert(equis))) ,Ripples_OR_N_S,'UniformOutput',false);
+z_or_n_s = vertcat(z_or_n_s{:});
+z_or_nn_s = cellfun(@(equis) max(abs(hilbert(equis))) ,Ripples_OR_NN_S,'UniformOutput',false);
+z_or_nn_s = vertcat(z_or_nn_s{:});
+
 
 % Area under curve
 l_or = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_OR,'UniformOutput',false));
-% l_hc = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_HC,'UniformOutput',false));
-% l_od = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_OD,'UniformOutput',false));
-% l_cn = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_CN,'UniformOutput',false));
+l_hc = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_HC,'UniformOutput',false));
+l_od = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_OD,'UniformOutput',false));
+l_cn = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_CN,'UniformOutput',false));
+l_or_n = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_OR_N,'UniformOutput',false));
+l_or_n_s = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_OR_N_S,'UniformOutput',false));
+l_or_nn_s = cell2mat(cellfun(@(equis) trapz((1:length(equis))./2500,abs(equis)), Ripples_OR_NN_S,'UniformOutput',false));
 
 % Duration
 q_or =(cellfun('length',Ripples_OR)/2500);
-% q_hc = (cellfun('length',Ripples_HC)/2500);
-% q_od =(cellfun('length',Ripples_OD)/2500);
-% q_cn = (cellfun('length',Ripples_CN)/2500);
-% 
+q_hc = (cellfun('length',Ripples_HC)/2500);
+q_od =(cellfun('length',Ripples_OD)/2500);
+q_cn = (cellfun('length',Ripples_CN)/2500);
+q_or_n =(cellfun('length',Ripples_OR_N)/2500);
+q_or_n_s =(cellfun('length',Ripples_OR_N_S)/2500);
+q_or_nn_s =(cellfun('length',Ripples_OR_NN_S)/2500);
+
 % PeaktoPeak Amplitude
 p_or = cellfun(@peak2peak,Ripples_OR);
-% p_hc = cellfun(@peak2peak,Ripples_HC);
-% p_od = cellfun(@peak2peak,Ripples_OD);
-% p_cn = cellfun(@peak2peak,Ripples_CN);
+p_hc = cellfun(@peak2peak,Ripples_HC);
+p_od = cellfun(@peak2peak,Ripples_OD);
+p_cn = cellfun(@peak2peak,Ripples_CN);
+p_or_n = cellfun(@peak2peak,Ripples_OR_N);
+p_or_n_s = cellfun(@peak2peak,Ripples_OR_N_S);
+p_or_nn_s = cellfun(@peak2peak,Ripples_OR_NN_S);
 
 % Combination of characteristics 
-
-% l_hc = l_hc';
-% l_cn = l_cn';
-% l_od = l_od';
+try
+l_hc = l_hc';
+l_cn = l_cn';
+l_od = l_od';
 l_or = l_or';
+l_or_n = l_or_n';
+l_or_n_s = l_or_n_s';
+l_or_nn_s = l_or_nn_s';
 
-% q_hc = q_hc';
-% q_cn = q_cn';
-% q_od = q_od';
+q_hc = q_hc';
+q_cn = q_cn';
+q_od = q_od';
 q_or = q_or';
+q_or_n = q_or_n';
+q_or_n_s = q_or_n_s';
+q_or_nn_s = q_or_nn_s';
 
-% p_hc = p_hc';
-% p_cn = p_cn';
-% p_od = p_od';
+p_hc = p_hc';
+p_cn = p_cn';
+p_od = p_od';
 p_or = p_or';
+p_or_n = p_or_n';
+p_or_n_s = p_or_n_s';
+p_or_nn_s = p_or_nn_s';
 
-% totalCharacteristicsHC= [x_hc y_hc z_hc l_hc q_hc p_hc];
-% totalCharacteristicsCN= [x_cn y_cn z_cn l_cn q_cn p_cn];
-% totalCharacteristicsOD= [x_od y_od z_od l_od q_od p_od];
+totalCharacteristicsHC= [x_hc y_hc z_hc l_hc q_hc p_hc];
+totalCharacteristicsCN= [x_cn y_cn z_cn l_cn q_cn p_cn];
+totalCharacteristicsOD= [x_od y_od z_od l_od q_od p_od];
 totalCharacteristicsOR= [x_or y_or z_or l_or q_or p_or];
-
+totalCharacteristicsOR_N= [x_or_n y_or_n z_or_n l_or_n q_or_n p_or_n];
+totalCharacteristicsOR_N_S= [x_or_n_s y_or_n_s z_or_n_s l_or_n_s q_or_n_s p_or_n_s];
+totalCharacteristicsOR_NN_S= [x_or_nn_s y_or_nn_s z_or_nn_s l_or_nn_s q_or_nn_s p_or_nn_s];
+catch 
+totalCharacteristicsHC= [x_hc y_hc z_hc l_hc q_hc p_hc];
+totalCharacteristicsCN= [x_cn y_cn z_cn l_cn q_cn p_cn];
+totalCharacteristicsOD= [x_od y_od z_od l_od q_od p_od];
+totalCharacteristicsOR= [x_or y_or z_or l_or q_or p_or];
+totalCharacteristicsOR_N= [x_or_n y_or_n z_or_n l_or_n q_or_n p_or_n];
+totalCharacteristicsOR_N_S= [x_or_n_s y_or_n_s z_or_n_s l_or_n_s q_or_n_s p_or_n_s];
+totalCharacteristicsOR_NN_S= [x_or_nn_s y_or_nn_s z_or_nn_s l_or_nn_s q_or_nn_s p_or_nn_s];
+end
 % subplot(3,2,1)
 % h1=histogram(x_or); title('Entropy');xlabel('Entropy');ylabel('Count')
 % h1.FaceColor= [1 1 1]; h1.LineWidth=1;
